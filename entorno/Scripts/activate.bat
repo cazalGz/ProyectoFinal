7f109c37bacfb92c8151f0622b22150220d2e907
cazalGz <giotapping@gmail.com>
2022-12-18 21:24:11 -0300

    cambio final

--- entorno/Scripts/activate.bat
+++ entorno/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Gonza\Desktop\ProyectoFinal\entorno
+set VIRTUAL_ENV=C:\Users\Gonza\Desktop\nuevo Proyecto\entorno
 
 if not defined PROMPT set PROMPT=$P$G
 
--- proyecto2022/paginaWeb/paginaWeb/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-from mysite.views import current_datetime
-
-
-urlpatterns = patterns('',
-   (r'^time/$', current_datetime),
-)
